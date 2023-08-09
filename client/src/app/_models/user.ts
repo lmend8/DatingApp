65593e748ac221c3363967fe00e76d6b6abe1dfc
lmend8 <luis.mendoza9122@gmail.com>
2023-08-09 11:49:19 -0600

    Error Status

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -2,3 +2,31 @@ export interface User {
     username: string;
     token : string;
 }
+
+let data: number | string = 42;
+
+data = "10";
+
+
+interface Car {
+    color:string;
+    model: string;
+    topSpeed:number;
+}
+
+const car1: Car ={
+    color:'blue',
+    model: 'BMW',
+    topSpeed: 23
+}
+
+const car2: Car={
+    color:'Red',
+    model: 'Mercedes',
+    topSpeed: 100
+}
+
+
+const multiply = (x: number,y: number) => {
+    return x * y;
+}
